2692cded4af0c397af4167fd6038696b59c41026
alexgpark <alexgpark@gmail.com>
2015-08-28 23:02:08 -0700

    created navbar and header styling

--- client/routes.js
+++ client/routes.js
@@ -1,7 +1,7 @@
 //inject the ngRoute dependency to the module
 var link = angular.module('link', ['ngRoute']);
 //Use the config method to set up routing
-myApp.config(function ($routeProvider) {
+link.config(function ($routeProvider) {
 	$routeProvider
 		.when('/', {
 			templateUrl: './partials/home.html'
