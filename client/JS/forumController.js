eb55d48e660abad9c005632c963d1a3fa3fc8078
Rui Wang <wangr3@gmail.com>
2015-08-31 10:27:06 -0700

    Update forumController.js

--- client/JS/forumController.js
+++ client/JS/forumController.js
@@ -33,41 +33,24 @@ link.controller('forumController', function($scope, $location, $compile, $routeP
 			//console.log('new post successfully saved');
 			$('#myModal').modal('hide');
 
-			// $( '#hidden-post label'  ).html( $scope.newPost.title );
-			// $( '#hidden-post label'  ).attr('for', 'id'+data._id);
-			// $( '#hidden-post .post'  ).attr('id', 'id'+data._id);			
-			// $( '#hidden-post .description').html("<span class='name-span'>"+$scope.newPost.userName + "</span> : "+data.description );
-			// $( '#hidden-post form '  ).attr('ng-submit', "addComment('"+data._id+"')");		
-	  //   	var div = $('#hidden-post');
-	  //   	var tmp = div.clone().attr('id', "post-"+ data._id );
-   //          $( '#hidden-post label'  ).html( "" );
-   //          var temp = $compile(tmp)($scope);
-            //angular.element( $('#forum-posts').prepend(temp) );
-
-
-
-            if ( $scope.posts == undefined ){
-            	console.log('here----->000');
-            	$scope.posts = [];
-            	$scope.posts.push( 
-
-            		{ "_id" :data._id , "userName" :  data.userName, "title" :  data.title,  "description" : data.description,  "updated_at" : data.updated_at, "created_at" : data.created_at }
-            
-            	 );
-            }
-            else {
-            	console.log('here----->222');
-            	$scope.posts.push( 
-
-            			{ "_id" :data._id , "userName" :  data.userName, "title" :  data.title,  "description" : data.description,  "updated_at" : data.updated_at, "created_at" : data.created_at }
-
-            		);
-            }
-
-
-
-            $scope.newPost = {};
-
+	            if ( $scope.posts == undefined ){
+	            	console.log('here----->000');
+	            	$scope.posts = [];
+	            	$scope.posts.push( 
+	
+	            		{ "_id" :data._id , "userName" :  data.userName, "title" :  data.title,  "description" : data.description,  "updated_at" : data.updated_at, "created_at" : data.created_at }
+	            
+	            	 );
+	            }
+	            else {
+	            	console.log('here----->222');
+	            	$scope.posts.push( 
+	
+	            			{ "_id" :data._id , "userName" :  data.userName, "title" :  data.title,  "description" : data.description,  "updated_at" : data.updated_at, "created_at" : data.created_at }
+	
+	            		);
+	            }
+	            $scope.newPost = {};
 		});
 
 	}
@@ -89,15 +72,6 @@ link.controller('forumController', function($scope, $location, $compile, $routeP
 		}
 		else {
 
-			//console.log( "+++++++++++", id );
-
-			//console.log( $('#'+event.target.attributes[0].nodeValue ).parent().siblings().children().children() );
-			// $('#'+event.target.attributes[0].nodeValue ).parent().siblings().children().children().toggle();
-
-			// $('#'+event.target.attributes[0].nodeValue ).parent().siblings().children(':nth-child(4)').hide();
-			// $('#'+event.target.attributes[0].nodeValue ).parent().siblings().children(':nth-child(5)').hide();
-			// $('#'+event.target.attributes[0].nodeValue ).parent().siblings().children(':nth-child(6)').hide();
-			// $('#'+event.target.attributes[0].nodeValue ).siblings().show();
 
 			$('#'+event.target.attributes[0].nodeValue ).parent().siblings().children(':nth-child(1)').removeAttr("checked");
 			//checkBoxes.attr("checked", !checkBoxes.attr("checked"));
@@ -115,11 +89,6 @@ link.controller('forumController', function($scope, $location, $compile, $routeP
 				//console.log('inside hashset'+id);
 				$scope.getComments(event.target.attributes[0].nodeValue);
 
-				// var div = $('.hidden-post-content');
-			 //    var tmp = div.clone().attr('class', "post-content" );
-			 //    console.log(tmp);
-			 //    var temp = $compile(tmp)($scope);
-				// angular.element( $('#'+id).append(tmp) );
 				$scope.postHashSet[id] = 1;
 			}
 
@@ -131,47 +100,38 @@ link.controller('forumController', function($scope, $location, $compile, $routeP
 
 
 	$scope.addComment = function(id){
-		//console.log("====="+id);
-		//console.log('adding comment...');
-		//console.log($scope.newComment);
-		//console.log(event)
 
 
 		$scope.newComment.post_id = id;
 		//$scope.newComment.created_at = event.timeStamp;
 		forumFactory.addComment($scope.newComment, function(data){
 
-			//these 4 lines of codes are used to manual changing html
-			// $( '#hidden-post  .comments .comment'  ).html( "<strong>"+$scope.newComment.userName+"</strong>"+" says:<br>"+$scope.newComment.comment);
-	  //   	var div = $('#hidden-post .comments .comment');
-	  //   	var tmp = div.clone();
-   //          $( '#post-'+data.post_id+'  .comments '  ).append(tmp );
 
-   			console.dir( $scope.newComment);
+   		console.dir( $scope.newComment);
 
-            //$( '#hidden-post .comments .comment'  ).html( "" );
-            if ( $scope.comments == undefined ){
-            	console.log('here----->');
-            	$scope.comments = [];
-            	$scope.comments.unshift( 
-
-            		{ "_id" :data._id , "userName" :  $scope.newComment.userName, "comment" : $scope.newComment.comment, "post_id" : data.post_id , "updated_at" : data.updated_at, "created_at" :data.created_at }
-            
-            	 );
-            }
-            else {
-            	console.log('here----->111');
-            	$scope.comments.unshift( 
-
-
-            		{ "_id" :data._id , "userName" :  $scope.newComment.userName, "comment" : $scope.newComment.comment, "post_id" : data.post_id , "updated_at" : data.updated_at, "created_at" :data.created_at }
-
-
-            		);
-            }
-
-            $scope.newComment = "";
-
+	            //$( '#hidden-post .comments .comment'  ).html( "" );
+	            if ( $scope.comments == undefined ){
+	            	console.log('here----->');
+	            	$scope.comments = [];
+	            	$scope.comments.unshift( 
+	
+	            		{ "_id" :data._id , "userName" :  $scope.newComment.userName, "comment" : $scope.newComment.comment, "post_id" : data.post_id , "updated_at" : data.updated_at, "created_at" :data.created_at }
+	            
+	            	 );
+	            }
+	            else {
+	            	console.log('here----->111');
+	            	$scope.comments.unshift( 
+	
+	
+	            		{ "_id" :data._id , "userName" :  $scope.newComment.userName, "comment" : $scope.newComment.comment, "post_id" : data.post_id , "updated_at" : data.updated_at, "created_at" :data.created_at }
+	
+	
+	            		);
+	            }
+	
+	            $scope.newComment = "";
+	
 		})
 
 
